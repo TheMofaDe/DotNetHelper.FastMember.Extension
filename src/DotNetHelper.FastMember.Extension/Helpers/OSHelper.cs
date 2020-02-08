733111a35f75bab8ca47c6c44b5b25d500fe1eb0
Joseph McNeal Jr <TheMofaDe@users.noreply.github.com>
2020-02-08 15:04:48 -0500

    added support for IOS by using reflection

--- src/DotNetHelper.FastMember.Extension/Helpers/OSHelper.cs
+++ src/DotNetHelper.FastMember.Extension/Helpers/OSHelper.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿#if NETSTANDARD
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
 namespace DotNetHelper.FastMember.Extension.Helpers
 {
-    public class OSHelper
+
+    public static class OSHelper
     {
-        public OSHelper()
+
+        public static bool IsRunningOnIOS { get; }
+        static OSHelper()
         {
+            IsRunningOnIOS = File.Exists(@"/System/Library/CoreServices/SystemVersion.plist") || System.Runtime.InteropServices.RuntimeInformation
+                                             .IsOSPlatform(OSPlatform.OSX);
         }
     }
 }
+
+#endif
